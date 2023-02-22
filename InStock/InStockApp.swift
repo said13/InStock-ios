f8d88ee4af5e3d2d6a947b9ec8beba061687e4bf
saidReclip <said@reclip.app>
2023-02-22 16:04:45 +0300

    Initial Commit

---
+++ InStock/InStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  InStockApp.swift
+//  InStock
+//
+//  Created by Abdullah Atkaev on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct InStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
